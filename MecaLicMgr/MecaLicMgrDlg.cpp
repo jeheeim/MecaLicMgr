08cb5823e283e2c9df431f7885d68b51ed737726
jay <jay@LAPTOP-JAY>
2016-07-09 13:58:35 +0900

    초기 커밋.

---
+++ MecaLicMgr/MecaLicMgrDlg.cpp
@@ -0,0 +1,294 @@
+
+// MecaLicMgrDlg.cpp : 구현 파일
+//
+
+/*
+1. OnInitDialog 메소드
+
+2. 전체 정보 읽기 버튼, 회사 정보 읽기 메소드, 사용자 정보 읽기 메소드 구현.
+OnButtReadAll, OnButtReadComp, OnButtReadUser 메소드
+
+3. 라이센스 읽기, 발급, 종료 메소드
+라이센스 읽기, 발급 메소드는 메시지박스, 종료는 프로그램 종료.
+
+4. ipv6 체크박스 메소드
+체크하면 12자리 6박스, 해제하면 8자리 4박스.
+*/
+
+#include "stdafx.h"
+#include "MecaLicMgr.h"
+#include "MecaLicMgrDlg.h"
+#include "afxdialogex.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// 응용 프로그램 정보에 사용되는 CAboutDlg 대화 상자입니다.
+
+class CAboutDlg : public CDialogEx
+{
+public:
+	CAboutDlg();
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_ABOUTBOX };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+
+// 구현입니다.
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+CAboutDlg::CAboutDlg() : CDialogEx(IDD_ABOUTBOX)
+{
+}
+
+void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CMecaLicMgrDlg 대화 상자
+
+
+
+CMecaLicMgrDlg::CMecaLicMgrDlg(CWnd* pParent /*=NULL*/)
+	: CDialogEx(IDD_MECALICMGR_DIALOG, pParent)
+	, check_ipv(FALSE)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CMecaLicMgrDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+	DDX_Check(pDX, IDC_CHECK_IPV6, check_ipv);
+}
+
+BEGIN_MESSAGE_MAP(CMecaLicMgrDlg, CDialogEx)
+	ON_WM_SYSCOMMAND()
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+	ON_BN_CLICKED(IDC_BUTT_EXIT, &CMecaLicMgrDlg::OnButtExit)
+	ON_BN_CLICKED(IDC_CHECK_IPV6, &CMecaLicMgrDlg::OnClickIpv6)
+	ON_BN_CLICKED(IDC_BUTT_READ_ALL, &CMecaLicMgrDlg::OnButtReadAll)
+	ON_BN_CLICKED(IDC_BUTT_READ_COMP, &CMecaLicMgrDlg::OnButtReadComp)
+	ON_BN_CLICKED(IDC_BUTT_READ_USER, &CMecaLicMgrDlg::OnButtReadUser)
+	ON_BN_CLICKED(IDC_BUTT_LIC_MAKE, &CMecaLicMgrDlg::OnButtLicMake)
+	ON_BN_CLICKED(IDC_BUTT_LIC_READ, &CMecaLicMgrDlg::OnButtLicRead)
+END_MESSAGE_MAP()
+
+
+// CMecaLicMgrDlg 메시지 처리기
+
+BOOL CMecaLicMgrDlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// 시스템 메뉴에 "정보..." 메뉴 항목을 추가합니다.
+
+	// IDM_ABOUTBOX는 시스템 명령 범위에 있어야 합니다.
+	ASSERT((IDM_ABOUTBOX & 0xFFF0) == IDM_ABOUTBOX);
+	ASSERT(IDM_ABOUTBOX < 0xF000);
+
+	CMenu* pSysMenu = GetSystemMenu(FALSE);
+	if (pSysMenu != NULL)
+	{
+		BOOL bNameValid;
+		CString strAboutMenu;
+		bNameValid = strAboutMenu.LoadString(IDS_ABOUTBOX);
+		ASSERT(bNameValid);
+		if (!strAboutMenu.IsEmpty())
+		{
+			pSysMenu->AppendMenu(MF_SEPARATOR);
+			pSysMenu->AppendMenu(MF_STRING, IDM_ABOUTBOX, strAboutMenu);
+		}
+	}
+
+	// 이 대화 상자의 아이콘을 설정합니다.  응용 프로그램의 주 창이 대화 상자가 아닐 경우에는
+	//  프레임워크가 이 작업을 자동으로 수행합니다.
+	SetIcon(m_hIcon, TRUE);			// 큰 아이콘을 설정합니다.
+	SetIcon(m_hIcon, FALSE);		// 작은 아이콘을 설정합니다.
+
+	// TODO: 여기에 추가 초기화 작업을 추가합니다.
+
+
+
+	return TRUE;  // 포커스를 컨트롤에 설정하지 않으면 TRUE를 반환합니다.
+}
+
+void CMecaLicMgrDlg::OnSysCommand(UINT nID, LPARAM lParam)
+{
+	if ((nID & 0xFFF0) == IDM_ABOUTBOX)
+	{
+		CAboutDlg dlgAbout;
+		dlgAbout.DoModal();
+	}
+	else
+	{
+		CDialogEx::OnSysCommand(nID, lParam);
+	}
+}
+
+// 대화 상자에 최소화 단추를 추가할 경우 아이콘을 그리려면
+//  아래 코드가 필요합니다.  문서/뷰 모델을 사용하는 MFC 응용 프로그램의 경우에는
+//  프레임워크에서 이 작업을 자동으로 수행합니다.
+
+void CMecaLicMgrDlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // 그리기를 위한 디바이스 컨텍스트입니다.
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// 클라이언트 사각형에서 아이콘을 가운데에 맞춥니다.
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// 아이콘을 그립니다.
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialogEx::OnPaint();
+	}
+}
+
+// 사용자가 최소화된 창을 끄는 동안에 커서가 표시되도록 시스템에서
+//  이 함수를 호출합니다.
+HCURSOR CMecaLicMgrDlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
+
+//여기서부터 작업중인 메소드.
+
+// 프로그램 종료 버튼
+void CMecaLicMgrDlg::OnButtExit()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+
+	OnOK();
+}
+
+// 체크하면 ipv6, 해제하면 ipv4. ip입력칸의 길이가 변경된다.
+void CMecaLicMgrDlg::OnClickIpv6()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+	
+	UpdateData(TRUE);
+	
+	if (check_ipv)
+	{
+		GetDlgItem(IDC_EDIT_MAC5)->ShowWindow(TRUE);
+		GetDlgItem(IDC_EDIT_MAC6)->ShowWindow(TRUE);
+		GetDlgItem(IDC_STATIC_MAC_BAR4)->ShowWindow(TRUE);
+		GetDlgItem(IDC_STATIC_MAC_BAR5)->ShowWindow(TRUE);
+	}
+
+	else
+	{
+		GetDlgItem(IDC_EDIT_MAC5)->ShowWindow(FALSE);
+		GetDlgItem(IDC_EDIT_MAC6)->ShowWindow(FALSE);
+		GetDlgItem(IDC_STATIC_MAC_BAR4)->ShowWindow(FALSE);
+		GetDlgItem(IDC_STATIC_MAC_BAR5)->ShowWindow(FALSE);
+	}
+}
+
+// 전체 정보 읽기 버튼.
+void CMecaLicMgrDlg::OnButtReadAll()
+{
+	BOOL bTrue = FALSE;
+	CString strTemp;
+
+	// txt파일. 라이센스 파일로 수정필요
+	CFileDialog dlgFileOpen(TRUE, "TXT", NULL, OFN_FILEMUSTEXIST, "메모장(*.txt)|*.txt|모든파일(*.*)|*.*||", NULL);
+
+	if (dlgFileOpen.DoModal() == IDOK)
+	{
+		MessageBox("전체 파일을 여는데 성공했습니다!", "성공", NULL);
+	}
+	else
+	{
+		MessageBox("전체 파일을 여는데 실패했습니다!", "실패", NULL);
+	}
+}
+
+// 회사 정보 읽기 버튼.
+void CMecaLicMgrDlg::OnButtReadComp()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+
+	BOOL bTrue = FALSE;
+	CString strTemp;
+
+	// 열기창. txt파일과 모든파일중에 선택할 수 있다.
+	CFileDialog dlgFileOpen(TRUE, "TXT", NULL, OFN_FILEMUSTEXIST, "메모장(*.txt)|*.txt|모든파일(*.*)|*.*||", NULL);
+
+	dlgFileOpen.m_ofn.lpstrInitialDir = (LPSTR)("C:\\Users\\jay\\Desktop\\MecaLicMgr\\data\\license\\comp");
+
+	if (dlgFileOpen.DoModal() == IDOK)
+	{
+		MessageBox("전체 파일을 여는데 성공했습니다!", "성공", NULL);
+	}
+	else
+	{
+		MessageBox("전체 파일을 여는데 실패했습니다!", "실패", NULL);
+	}
+}
+
+//사용자 정보 읽기 버튼.
+void CMecaLicMgrDlg::OnButtReadUser()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+
+	BOOL bTrue = FALSE;
+	CString strTemp;
+
+	// 열기창. txt파일과 모든파일중에 선택할 수 있다.
+	CFileDialog dlgFileOpen(TRUE, "TXT", NULL, OFN_FILEMUSTEXIST, "메모장(*.txt)|*.txt|모든파일(*.*)|*.*||", NULL);
+
+	// 유저폴더.
+	dlgFileOpen.m_ofn.lpstrInitialDir = (LPSTR)("C:\\Users\\jay\\Desktop\\MecaLicMgr\\data\\license\\user");
+
+	if (dlgFileOpen.DoModal() == IDOK)
+	{
+		MessageBox("전체 파일을 여는데 성공했습니다!", "성공", NULL);
+	}
+	else
+	{
+		MessageBox("전체 파일을 여는데 실패했습니다!", "실패", NULL);
+	}
+}
+
+
+void CMecaLicMgrDlg::OnButtLicMake()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+
+	MessageBox("라이센스를 발급합니다.", "발급", NULL);
+}
+
+
+void CMecaLicMgrDlg::OnButtLicRead()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+
+	MessageBox("라이센스를 불러옵니다.", "읽기", NULL);
+}
---
+++ MecaLicMgr/MecaLicMgrDlg.h
@@ -0,0 +1,45 @@
+
+// MecaLicMgrDlg.h : 헤더 파일
+//
+
+#pragma once
+
+
+// CMecaLicMgrDlg 대화 상자
+class CMecaLicMgrDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CMecaLicMgrDlg(CWnd* pParent = NULL);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MECALICMGR_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnButtExit();
+	afx_msg void OnClickIpv6();
+private:
+	BOOL check_ipv;
+public:
+	afx_msg void OnButtReadAll();
+	afx_msg void OnButtReadComp();
+	afx_msg void OnButtReadUser();
+	afx_msg void OnButtLicMake();
+	afx_msg void OnButtLicRead();
+};
